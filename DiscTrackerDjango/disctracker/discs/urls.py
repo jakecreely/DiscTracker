7a509402bda8f86b233e8e25667b2fdf2d6c7b54
Jake <55953689+jakecreely@users.noreply.github.com>
2025-01-24 15:57:45 +0000

    Basic views and urls

--- DiscTrackerDjango/disctracker/discs/urls.py
+++ DiscTrackerDjango/disctracker/discs/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    # ex: /discs/1
+    path("<int:item_id>/", views.detail, name="detail"),
+    # ex: /discs/price-history/1 
+    path("price-history/<int:item_id>/", views.price_history, name="price-history"),
 ]
--- DiscTrackerDjango/disctracker/discs/views.py
+++ DiscTrackerDjango/disctracker/discs/views.py
@@ -2,4 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, world. You're at the disc index.")
+
+def detail(request, item_id):
+    return HttpResponse("You're looking at item %s." % item_id)
+
+def price_history(request, item_id):
+    response = "You're looking at the price history of item %s."
+    return HttpResponse(response % item_id)
