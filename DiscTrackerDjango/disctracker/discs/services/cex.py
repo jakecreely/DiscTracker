f3d65d2183fff9dc9c7491b7cd6c9a9276aee933
Jake <55953689+jakecreely@users.noreply.github.com>
2025-01-26 23:02:01 +0000

    Added error handling to cex service

--- DiscTrackerDjango/disctracker/discs/services/cex.py
+++ DiscTrackerDjango/disctracker/discs/services/cex.py
@@ -4,31 +4,35 @@ from datetime import datetime
 from ..models import Item, PriceHistory
 
 def fetch_item(cex_id):
-    search_url = f'https://wss2.cex.uk.webuy.io/v3/boxes/{cex_id}/detail'
-    response = requests.get(search_url)
-    if response.status_code == 200:
-        try:
-            data = response.json()
-            return data['response']['data']
-        except:
-            print('Failed to parse JSON response')
-            return None
-    else:
-        print('Failed To Fetch Item')
+    try:
+        search_url = f'https://wss2.cex.uk.webuy.io/v3/boxes/{cex_id}/detail'
+        response = requests.get(search_url)
+        response.raise_for_status()
+        data = response.json()
+        return data['response']['data']
+    except requests.exceptions.HTTPError as e:
+        print(f"HTTP Error when fetching item by CEX ID: {e}")
+        return None
+    except requests.exceptions.JSONDecodeError as e:
+        print(f"Failed to parse JSON response for CEX ID {cex_id}: {e}")
+        return None
+    except Exception as e:
+        print(f"An unexpected error occurred for fetching item by CEX ID {cex_id}: {e}")
         return None
 
 def check_price_updates():
-    items = Item.objects.all()
+    try: 
+        items = Item.objects.all()
 
-    for item in items:
-        cex_id = item.cex_id
-        current_sell_price = item.sell_price
-        current_exchange_price = item.exchange_price
-        current_cash_price = item.cash_price
+        for item in items:
+            cex_id = item.cex_id
+            # current_sell_price = item.sell_price
+            current_exchange_price = item.exchange_price
+            current_cash_price = item.cash_price
 
-        response = requests.get(f'https://wss2.cex.uk.webuy.io/v3/boxes/{cex_id}/detail')
-        
-        if response.status_code == 200:
+            response = requests.get(f'https://wss2.cex.uk.webuy.io/v3/boxes/{cex_id}/detail')
+            response.raise_for_status()
+            
             data = response.json()['response']['data']
             new_sell_price = data['boxDetails'][0]['sellPrice']
             new_cash_price = data['boxDetails'][0]['cashPrice']
@@ -56,7 +60,13 @@ def check_price_updates():
                     cash_price=new_cash_price,
                     date_checked=datetime.now(),
                 )                
-            else:
-                print(f"Price not found for ID {cex_id}")
-        else:
-            print(f"Failed to fetch data for ID {cex_id}")
+        return "Price updates completed successfully."
+    except requests.exceptions.HTTPError as e:
+        print(f"HTTP Error when fetching item by CEX ID: {e}")
+        return None
+    except requests.exceptions.JSONDecodeError as e:
+        print(f"Failed to parse JSON response for CEX ID {cex_id}: {e}")
+        return None
+    except Exception as e:
+        print(f"An unexpected error occurred for checking item prices: {e}")
+        return None
