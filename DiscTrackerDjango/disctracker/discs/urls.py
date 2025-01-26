8d957f7c05ec0b32601952b98cb4e835ec1c7340
Jake <55953689+jakecreely@users.noreply.github.com>
2025-01-26 23:03:42 +0000

    Route example comments

--- DiscTrackerDjango/disctracker/discs/urls.py
+++ DiscTrackerDjango/disctracker/discs/urls.py
@@ -4,13 +4,18 @@ from . import views
 
 app_name="discs"
 urlpatterns = [
+    # ex: /discs/    
     path("", views.index, name="index"),
+    
     # ex: /discs/1
     path("<str:item_id>/", views.detail, name="detail"),
+    
     # ex: /discs/price-history/1 
     path("price-history/<str:item_id>/", views.price_history, name="price-history"),
-    
+
+    # ex: /discs/add-item
     path("add-item", views.add_item_from_cex, name="add-item"),
     
+    # ex: /discs/update-item-prices
     path("update-item-prices", views.update_item_prices, name="update-item-prices")
 ]
