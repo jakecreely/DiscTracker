3e2efefe6e04abf35f1dae5de999ddb80ab9500a
Jake <55953689+jakecreely@users.noreply.github.com>
2025-01-27 00:38:04 +0000

    Added master template

--- DiscTrackerDjango/disctracker/disctracker/settings.py
+++ DiscTrackerDjango/disctracker/disctracker/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'disctracker.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "disctracker/templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
