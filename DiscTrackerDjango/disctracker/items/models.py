1a8c345ecc5523e3f5f986c4383864b5b3e6e866
Jake <55953689+jakecreely@users.noreply.github.com>
2025-02-06 22:04:49 +0000

    Validation for Price History Model

--- DiscTrackerDjango/disctracker/items/models.py
+++ DiscTrackerDjango/disctracker/items/models.py
@@ -37,10 +37,28 @@ class Item(models.Model):
 
 class PriceHistory(models.Model):
     item = models.ForeignKey(Item, related_name='price_history', on_delete=models.CASCADE)
-    sell_price = models.FloatField()
-    exchange_price = models.FloatField()
-    cash_price = models.FloatField()
     date_checked = models.DateField(default=datetime.now)
+    sell_price = models.DecimalField(
+        max_digits=10, 
+        decimal_places=2, 
+        validators=[
+            MinValueValidator(0, "Sell Price must be greater or equal to 0"), 
+            MaxValueValidator(3000, "Sell Price must be less than or equal to 3000")
+        ])
+    exchange_price = models.DecimalField(
+        max_digits=10, 
+        decimal_places=2, 
+        validators=[
+            MinValueValidator(0, "Exchange Price must be greater or equal to 0"), 
+            MaxValueValidator(3000, "Exchange Price must be less than or equal to 3000")
+        ])
+    cash_price = models.DecimalField(
+        max_digits=10, 
+        decimal_places=2, 
+        validators=[
+            MinValueValidator(0, "Cash Price must be greater or equal to 0"), 
+            MaxValueValidator(3000, "Cash Price must be less than or equal to 3000")
+        ])
 
     def __str__(self):
         return f"Price History for {self.item.title} on {self.date_checked}"
