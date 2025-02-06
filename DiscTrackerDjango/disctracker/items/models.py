80f9999bc1fa515cd1794a3c9463f43436ce2d4c
Jake <55953689+jakecreely@users.noreply.github.com>
2025-02-06 22:05:30 +0000

    Changed from datetime.now to date.today for both models

--- DiscTrackerDjango/disctracker/items/models.py
+++ DiscTrackerDjango/disctracker/items/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from datetime import datetime
+from datetime import date
 from django.core.validators import MinValueValidator, MaxValueValidator, RegexValidator
 
 class Item(models.Model):
@@ -9,7 +9,6 @@ class Item(models.Model):
             RegexValidator(r'^[A-Za-z0-9]+$', 'ID must be alphanumeric characters')
         ])
     title = models.CharField(max_length=255)
-    last_checked = models.DateField()
     sell_price = models.DecimalField(
         max_digits=10, 
         decimal_places=2, 
@@ -31,13 +30,13 @@ class Item(models.Model):
             MinValueValidator(0, "Cash Price must be greater or equal to 0"), 
             MaxValueValidator(3000, "Cash Price must be less than or equal to 3000")
         ])
+    last_checked = models.DateField(default=date.today)
 
     def __str__(self):
         return self.title
 
 class PriceHistory(models.Model):
     item = models.ForeignKey(Item, related_name='price_history', on_delete=models.CASCADE)
-    date_checked = models.DateField(default=datetime.now)
     sell_price = models.DecimalField(
         max_digits=10, 
         decimal_places=2, 
@@ -59,6 +58,7 @@ class PriceHistory(models.Model):
             MinValueValidator(0, "Cash Price must be greater or equal to 0"), 
             MaxValueValidator(3000, "Cash Price must be less than or equal to 3000")
         ])
+    date_checked = models.DateField(default=date.now)
 
     def __str__(self):
         return f"Price History for {self.item.title} on {self.date_checked}"
