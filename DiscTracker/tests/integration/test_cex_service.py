38a9765afa906b6dafad82af3ee98c67b32dcd2e
Jake <55953689+jakecreely@users.noreply.github.com>
2025-03-06 12:15:01 +0000

    Integration Tests for Cex Service Delete Item

--- DiscTracker/tests/integration/test_cex_service.py
+++ DiscTracker/tests/integration/test_cex_service.py
@@ -369,6 +369,179 @@ class TestCexServiceCreateOrUpdateItem(TestCase):
         self.assertTrue(user_two_item_exists)
 
 
+class TestCexServiceDeleteItem(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username="testuser", password="testpass"
+        )
+
+        self.existing_item = Item.objects.create(
+            cex_id="5060020626449",
+            title="Halloween (18) 1978",
+            sell_price=8.0,
+            exchange_price=5.0,
+            cash_price=3.0,
+            last_checked=date(2025, 1, 1),
+        )
+
+        self.user_existing_item = UserItem.objects.get_or_create(
+            user=self.user,
+            item=self.existing_item,
+        )
+
+        self.valid_fetched_item_data = {
+            "boxId": "5050582577013",
+            "boxName": "Thing, The (18) 1982",
+            "sellPrice": 6.0,
+            "exchangePrice": 2.0,
+            "cashPrice": 1.5,
+        }
+
+    def test_delete_item_success(self):
+        user_item_already_exists = UserItem.objects.filter(
+            user=self.user, item=self.existing_item
+        ).exists()
+        self.assertTrue(user_item_already_exists)
+
+        item_deleted = cex.delete_item(self.existing_item.cex_id, self.user)
+
+        self.assertTrue(item_deleted)
+
+        user_item_exists = UserItem.objects.filter(
+            user=self.user, item=self.existing_item
+        ).exists()
+        self.assertFalse(user_item_exists)
+
+    def test_delete_item_id_is_none(self):
+        item_deleted = cex.delete_item(None, self.user)
+
+        self.assertFalse(item_deleted)
+
+        user_item_exists = UserItem.objects.filter(
+            user=self.user, item=self.existing_item
+        ).exists()
+        self.assertTrue(user_item_exists)
+
+    def test_delete_item_invalid_id(self):
+        item_deleted = cex.delete_item("-1", self.user)
+
+        self.assertFalse(item_deleted)
+
+        user_item_exists = UserItem.objects.filter(
+            user=self.user, item=self.existing_item
+        ).exists()
+        self.assertTrue(user_item_exists)
+
+    def test_delete_item_invalid_user(self):
+        item_deleted = cex.delete_item(self.existing_item.cex_id, "invalid_user")
+
+        self.assertFalse(item_deleted)
+
+        user_item_exists = UserItem.objects.filter(
+            user=self.user, item=self.existing_item
+        ).exists()
+        self.assertTrue(user_item_exists)
+
+    @patch("items.models.db_models.Item.objects.get")
+    def test_delete_item_item_is_none(self, mock_get):
+        mock_get.side_effect = None
+
+        item_deleted = cex.delete_item(self.existing_item.cex_id, self.user)
+
+        self.assertFalse(item_deleted)
+
+        user_item_exists = UserItem.objects.filter(
+            user=self.user, item=self.existing_item
+        ).exists()
+        self.assertTrue(user_item_exists)
+
+    def test_delete_item_no_useritem_relation(self):
+        UserItem.objects.filter(user=self.user, item=self.existing_item).delete()
+
+        item_deleted = cex.delete_item(self.existing_item.cex_id, self.user)
+
+        self.assertFalse(item_deleted)
+
+        user_item_exists = UserItem.objects.filter(
+            user=self.user, item=self.existing_item
+        ).exists()
+        self.assertFalse(user_item_exists)
+
+    @patch("items.models.db_models.UserItem.delete")
+    def test_delete_item_deleted_count_eq_0(self, mock_delete):
+        mock_delete.return_value = (0, None)
+
+        item_deleted = cex.delete_item(self.existing_item.cex_id, self.user)
+
+        self.assertFalse(item_deleted)
+
+        user_item_exists = UserItem.objects.filter(
+            user=self.user, item=self.existing_item
+        ).exists()
+        self.assertTrue(user_item_exists)
+
+    @patch("items.models.db_models.UserItem.delete")
+    def test_delete_item_deleted_count_gt_1(self, mock_delete):
+        mock_delete.return_value = (3, None)
+
+        item_deleted = cex.delete_item(self.existing_item.cex_id, self.user)
+
+        self.assertFalse(item_deleted)
+
+        user_item_exists = UserItem.objects.filter(
+            user=self.user, item=self.existing_item
+        ).exists()
+        self.assertTrue(user_item_exists)
+
+    def test_delete_item_multiple_users(self):
+        user_1 = get_user_model().objects.create(username="user1", password="pass1")
+        user_2 = get_user_model().objects.create(username="user2", password="pass2")
+
+        UserItem.objects.create(user=user_1, item=self.existing_item)
+        UserItem.objects.create(user=user_2, item=self.existing_item)
+
+        item_deleted = cex.delete_item(self.existing_item.cex_id, user_1)
+
+        self.assertTrue(item_deleted)
+
+        user_item_1_exists = UserItem.objects.filter(
+            user=user_1, item=self.existing_item
+        ).exists()
+        user_item_2_exists = UserItem.objects.filter(
+            user=user_2, item=self.existing_item
+        ).exists()
+
+        # User 1 doesn't have item but User 2 still does
+        self.assertFalse(user_item_1_exists)
+        self.assertTrue(user_item_2_exists)
+
+    @patch("items.models.db_models.Item.objects.get")
+    def test_delete_item_database_error(self, mock_get):
+        mock_get.side_effect = DatabaseError
+
+        item_deleted = cex.delete_item(self.existing_item.cex_id, self.user)
+
+        self.assertFalse(item_deleted)
+
+        user_item_exists = UserItem.objects.filter(
+            user=self.user, item=self.existing_item
+        ).exists()
+        self.assertTrue(user_item_exists)
+
+    @patch("items.models.db_models.Item.objects.get")
+    def test_delete_item_unknown_exception(self, mock_get):
+        mock_get.side_effect = Exception
+
+        item_deleted = cex.delete_item(self.existing_item.cex_id, self.user)
+
+        self.assertFalse(item_deleted)
+
+        user_item_exists = UserItem.objects.filter(
+            user=self.user, item=self.existing_item
+        ).exists()
+        self.assertTrue(user_item_exists)
+
+
 class TestCexServiceCreatePriceHistoryEntry(TestCase):
     def setUp(self):
         self.user = get_user_model().objects.create_user(
