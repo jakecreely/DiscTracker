4e5ae4c852db8aa9c544928395f9df5e29376e9b
Jake <55953689+jakecreely@users.noreply.github.com>
2025-03-18 13:02:15 +0000

    Consistent sorting when ordering by including id

--- DiscTracker/items/filters.py
+++ DiscTracker/items/filters.py
@@ -1,22 +1,35 @@
 import django_filters
 from items.models.db_models import Item
 
+
 class ItemFilter(django_filters.FilterSet):
     ordering = django_filters.OrderingFilter(
         fields=(
-            ('title', 'title'),
-            ('sell_price', 'sell_price'),
-            ('exchange_price', 'exchange_price'),
-            ('cash_price', 'cash_price'),
+            ("title", "title"),
+            ("sell_price", "sell_price"),
+            ("exchange_price", "exchange_price"),
+            ("cash_price", "cash_price"),
         ),
-        label="Order by"
+        label="Order by",
     )
-    
+
     class Meta:
         model = Item
         fields = {
-            'title': ['icontains'],
-            'sell_price': ['lt', 'gt'],
-            'exchange_price': ['lt', 'gt'],
-            'cash_price': ['lt', 'gt'],
+            "title": ["icontains"],
+            "sell_price": ["lt", "gt"],
+            "exchange_price": ["lt", "gt"],
+            "cash_price": ["lt", "gt"],
         }
+
+    def filter_queryset(self, queryset):
+        queryset = super().filter_queryset(queryset)
+
+        order_by = self.data.get("ordering")
+
+        if order_by:
+            if order_by.startswith("-"):
+                queryset = queryset.order_by(order_by, "-id")  # Descending order
+            else:
+                queryset = queryset.order_by(order_by, "id")  # Ascending order
+        return queryset
